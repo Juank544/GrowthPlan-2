196f6276bbc578df95768782b50d693912c80c46
Juank544 <camilo.gv@live.com>
2023-04-26 10:07:51 -0500

    test(stadium): coverage for the stadiumService

--- src/main/java/co/com/perficient/project3/model/dto/StadiumDTO.java
+++ src/main/java/co/com/perficient/project3/model/dto/StadiumDTO.java
@@ -1,12 +1,16 @@
 package co.com.perficient.project3.model.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public class StadiumDTO implements Serializable {
     private String name;
     private String country;
