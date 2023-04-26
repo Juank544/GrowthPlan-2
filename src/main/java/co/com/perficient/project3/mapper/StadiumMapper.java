dcec402b87024d083992c407bbef897a7893d1fd
Juank544 <camilo.gv@live.com>
2023-04-26 11:17:09 -0500

    fix(stadium): correction to mapper and unused anotations

--- src/main/java/co/com/perficient/project3/mapper/StadiumMapper.java
+++ src/main/java/co/com/perficient/project3/mapper/StadiumMapper.java
@@ -2,13 +2,10 @@ package co.com.perficient.project3.mapper;
 
 import co.com.perficient.project3.model.dto.StadiumDTO;
 import co.com.perficient.project3.model.entity.Stadium;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface StadiumMapper {
     Stadium toEntity(StadiumDTO stadiumDTO);
-
-    @InheritInverseConfiguration
     StadiumDTO toDTO(Stadium stadium);
 }
--- src/main/java/co/com/perficient/project3/model/dto/StadiumDTO.java
+++ src/main/java/co/com/perficient/project3/model/dto/StadiumDTO.java
@@ -1,16 +1,12 @@
 package co.com.perficient.project3.model.dto;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
-@SuperBuilder
-@NoArgsConstructor
 public class StadiumDTO implements Serializable {
     private String name;
     private String country;
