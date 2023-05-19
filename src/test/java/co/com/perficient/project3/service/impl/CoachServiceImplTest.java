263f221914464f2ccb3b69cecc85dc3374b47827
Juank544 <camilo.gv@live.com>
2023-05-19 17:27:52 -0500

    refactor(): updating spring version

--- src/test/java/co/com/perficient/project3/service/impl/CoachServiceImplTest.java
+++ src/test/java/co/com/perficient/project3/service/impl/CoachServiceImplTest.java
@@ -20,7 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class CoachServiceImplTest {
 
@@ -33,7 +33,7 @@ class CoachServiceImplTest {
 
     @BeforeEach
     void setUp() {
-        initMocks(this);
+        openMocks(this);
     }
 
     @Test
