08b215d9130efae2cd58b00b11e9877849ada065
Juank544 <camilo.gv@live.com>
2023-06-20 16:36:58 -0500

    feat(coach): impl of HATEOAS

--- src/main/java/co/com/perficient/project3/controller/CoachController.java
+++ src/main/java/co/com/perficient/project3/controller/CoachController.java
@@ -5,6 +5,8 @@ import co.com.perficient.project3.model.dto.CoachDTO;
 import co.com.perficient.project3.model.entity.Coach;
 import co.com.perficient.project3.service.CoachService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +19,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static co.com.perficient.project3.utils.constant.CoachConstants.COACH;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = COACH, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -40,15 +45,25 @@ public class CoachController {
 
     @GetMapping
     public ResponseEntity<List<CoachDTO>> findAllCoaches() {
-        List<CoachDTO> coaches = coachService.findAll().stream().map(coachMapper::toDTO).toList();
+        List<CoachDTO> coaches = coachService.findAll().stream()
+                .map(coach -> EntityModel.of(coach, linkTo(methodOn(CoachController.class).findCoachById(coach.getId())).withSelfRel()))
+                .map(coachMapper::toDTO).toList();
         return new ResponseEntity<>(coaches, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CoachDTO> findCoachById(@PathVariable UUID id) {
         Optional<Coach> optionalCoach = coachService.findById(id);
-        return optionalCoach.map(coach -> new ResponseEntity<>(coachMapper.toDTO(coach), HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (optionalCoach.isPresent()) {
+            ArrayList<Link> links = new ArrayList<>();
+            links.add(linkTo(methodOn(CoachController.class).findAllCoaches()).withRel("allCoaches"));
+            optionalCoach.map(Coach::getTeam)
+                    .ifPresent(team -> links.add(linkTo(methodOn(TeamController.class).findTeamById(team.getId())).withRel("team")));
+
+            EntityModel<Coach> coachEntityModel = EntityModel.of(optionalCoach.get(), links);
+            return new ResponseEntity<>(coachMapper.toDTO(coachEntityModel), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
--- src/main/java/co/com/perficient/project3/mapper/CoachMapper.java
+++ src/main/java/co/com/perficient/project3/mapper/CoachMapper.java
@@ -4,6 +4,7 @@ import co.com.perficient.project3.model.dto.CoachDTO;
 import co.com.perficient.project3.model.entity.Coach;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.hateoas.EntityModel;
 
 @Mapper(uses = TeamMapper.class)
 public interface CoachMapper {
@@ -13,4 +14,10 @@ public interface CoachMapper {
 
     @Mapping(target = "team", source = "team.name")
     CoachDTO toDTO(Coach coach);
+
+    @Mapping(target = "name", source = "coachEntityModel.content.name")
+    @Mapping(target = "nationality", source = "coachEntityModel.content.nationality")
+    @Mapping(target = "birthDate", source = "coachEntityModel.content.birthDate")
+    @Mapping(target = "team", source = "coachEntityModel.content.team.name")
+    CoachDTO toDTO(EntityModel<Coach> coachEntityModel);
 }
