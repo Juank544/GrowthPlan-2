a94fc9da0014d72ca3c2dc39e7d3d60ef9403ecb
Juank544 <camilo.gv@live.com>
2023-06-21 11:25:54 -0500

    feat(player): impl of HATEOAS

--- src/main/java/co/com/perficient/project3/controller/PlayerController.java
+++ src/main/java/co/com/perficient/project3/controller/PlayerController.java
@@ -5,6 +5,8 @@ import co.com.perficient.project3.model.dto.PlayerDTO;
 import co.com.perficient.project3.model.entity.Player;
 import co.com.perficient.project3.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
 import static co.com.perficient.project3.utils.constant.PlayerConstants.PLAYER;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = PLAYER, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -42,18 +47,29 @@ public class PlayerController {
 
     @GetMapping
     public ResponseEntity<List<PlayerDTO>> findAllPlayers(@RequestParam(required = false) String team) {
-        List<PlayerDTO> players;
+        List<Player> players;
         if (Objects.isNull(team)) {
-            players = playerService.findAll().stream().map(playerMapper::toDTO).toList();
-        } else players = playerService.findAllByTeam(team).stream().map(playerMapper::toDTO).toList();
-        return new ResponseEntity<>(players, HttpStatus.OK);
+            players = playerService.findAll();
+        } else players = playerService.findAllByTeam(team);
+        List<PlayerDTO> playersDTO = players.stream()
+                .map(player -> EntityModel.of(player, linkTo(methodOn(PlayerController.class).findPlayerById(player.getId())).withSelfRel()))
+                .map(playerMapper::toDTO).toList();
+        return new ResponseEntity<>(playersDTO, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PlayerDTO> findPlayerById(@PathVariable UUID id) {
         Optional<Player> optionalPlayer = playerService.findById(id);
-        return optionalPlayer.map(player -> new ResponseEntity<>(playerMapper.toDTO(player), HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        if (optionalPlayer.isPresent()) {
+            List<Link> links = new ArrayList<>();
+            links.add(linkTo(PlayerController.class).withRel("allPlayers"));
+            optionalPlayer.map(Player::getTeam)
+                    .ifPresent(team -> links.add(linkTo(methodOn(TeamController.class).findTeamById(team.getId())).withRel("team")));
+
+            EntityModel<Player> playerEntityModel = EntityModel.of(optionalPlayer.get(), links);
+            return new ResponseEntity<>(playerMapper.toDTO(playerEntityModel), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
